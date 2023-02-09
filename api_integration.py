faf0e7174e183b28d573f91c9c27d6f40501350b
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-02-09 16:28:35 +0530

    adding quotes to string columns removed from the code

--- api_integration.py
+++ api_integration.py
@@ -6,7 +6,6 @@ import configparser
 import pandas as pd
 
 configuartion_path = os.path.dirname(os.path.abspath(__file__)) + "/transformer_generator/transformers/python_files/config.ini"
-# print(configuartion_path)
 config = configparser.ConfigParser()
 config.read(configuartion_path);
 # Creating the class

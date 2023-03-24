9a157bced59f22d52d5c21a08f1a8f8e0059529a
ramya-k-murthy <ramyakmurthy1119@gmail.com>
2023-03-24 14:26:09 +0530

    added dimension function

--- adapter/NISHTHA_consumption.py
+++ adapter/NISHTHA_consumption.py
@@ -3,6 +3,7 @@ from main import CollectData
 obj=CollectData()
 program=obj.program
 df_data=obj.get_file()
+
 def totalenrolment_event_data():
     df_snap = df_data[['State Code','District Code','Program', 'Total Enrollments']]
     df_snap.columns = ['state_id','district_id','program_name', 'total_enrolment']
@@ -30,9 +31,16 @@ def program_dimension_data():
     df_snap.columns = ['program_id', 'program_name']
     obj.upload_file(df_snap, 'nishthaprogram-dimension.data.csv')
 
+def district_dimension():
+    df_snap = df_data[['State Code','State Name','District Code','User District_Old','Latitude', 'Longitude']]
+    df_snap.columns = ['state_id','state_name','district_id','district_name','latitude','longitude']
+    obj.upload_file(df_snap, 'district-dimension.data.csv')
+
+
 if df_data is not None:
     totalenrolment_event_data()
     totalcompletion_event_data()
     totalcertification_event_data()
     perccertification_event_data()
     program_dimension_data()
+    district_dimension()
--- adapter/VSK_data_transformation.sh
+++ adapter/VSK_data_transformation.sh
@@ -1,19 +1,19 @@
-python DIKSHA_etb_coverage.py DIKSHA vsk_diksha_etb_coverage-status.zip
-python DIKSHA_etb_learning_session.py DIKSHA diksha_etb_learning-session.zip
-python DIKSHA_etb_plays_per_capita.py DIKSHA diksha_etb_plays-per-capita.zip
-python DIKSHA_etb_qr_coverage.py DIKSHA vsk_diksha_etb_qr-coverage.zip
-python NISHTHA_consumption.py NISHTHA diksha_nishtha_consumption-by-district.zip
-python NISHTHA_consumption_by_courses.py NISHTHA diksha_nishtha_consumption-by-course.zip
+python DIKSHA_etb_coverage.py diksha vsk_diksha_etb_coverage-status.zip
+python DIKSHA_etb_learning_session.py diksha diksha_etb_learning-session.zip
+python DIKSHA_etb_plays_per_capita.py diksha diksha_etb_plays-per-capita.zip
+python DIKSHA_etb_qr_coverage.py diksha vsk_diksha_etb_qr-coverage.zip
+python NISHTHA_consumption.py nishtha diksha_nishtha_consumption-by-district.zip
+python NISHTHA_consumption_by_courses.py nishtha diksha_nishtha_consumption-by-course.zip
 python NISHTHA_percentage_enrollment_certification.py NISHTHA diksha_nishtha_percentage-enrollment-certification.zip
-python NISHTHA_program_started.py NISHTHA diksha_nishtha_program-started.zip
-python NISHTHA_tot_courses_medium.py NISHTHA diksha_nishtha_tot-courses-medium.zip
-python NAS_all_dashboard.py NAS nas_all-dashboard.zip
-python NAS_program_started.py NAS nas_program-started.zip
-python PGI_all_dashboard.py PGI pgi_all-dashboard.zip
-python PGI_performance.py PGI  pgi_state-wise-performance.zip
-python PM_Poshan.py PM_Poshan pm-poshan_access-across-india.zip
-python UDISE_all_dashboard.py UDISE udise-all-dashboard.zip
-python UDISE_program_started.py UDISE udise_program-started.zip
+python NISHTHA_program_started.py nishtha diksha_nishtha_program-started.zip
+python NISHTHA_tot_courses_medium.py nishtha diksha_nishtha_tot-courses-medium.zip
+python NAS_all_dashboard.py nas nas_all-dashboard.zip
+python NAS_program_started.py nas nas_program-started.zip
+python PGI_all_dashboard.py pgi pgi_all-dashboard.zip
+python PGI_performance.py pgi  pgi_state-wise-performance.zip
+python PM_Poshan.py pm_poshan pm-poshan_access-across-india.zip
+python UDISE_all_dashboard.py udise udise-all-dashboard.zip
+python UDISE_program_started.py udise udise_program-started.zip
 
 
 
